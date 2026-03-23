3fb123e37bb3cf930751252aa532dce85850332a
flowerhahahaha <roderickscroggins647@gmail.com>
2026-03-23 11:07:59 +0800

    Update PostHero.js

--- themes/hexo/components/PostHero.js
+++ themes/hexo/components/PostHero.js
@@ -70,9 +70,10 @@ export default function PostHero({ post, siteInfo }) {
                   </SmartLink>
                 </>
               )}
-              <div className='pl-1 mr-2'>
-                {locale.COMMON.LAST_EDITED_TIME}: {post.lastEditedDay}
-              </div>
+              // 隐藏最后更新时间
+              // <div className='pl-1 mr-2'>
+              //   {locale.COMMON.LAST_EDITED_TIME}: {post.lastEditedDay}
+              // </div>
             </div>
 
             {JSON.parse(siteConfig('ANALYTICS_BUSUANZI_ENABLE')) && (
